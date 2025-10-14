7246f59efb3c4ff8e762f57f94d3fb4f189c2106
AzamatUzakov <azamatakmal053@gmail.com>
2025-10-14 16:00:35 +0500

    MaxConsecutiveOnes.js

---
+++ Easy/MaxConsecutiveOnes.js
@@ -0,0 +1,22 @@
+var findMaxConsecutiveOnes = function (nums) {
+
+
+    console.log(nums);
+    let maxLength = 0
+    let maxNum = 0
+    for (let i of nums) {
+
+        if (i === 1) {
+            maxLength++
+            if (maxLength > maxNum) {
+                maxNum = maxLength
+            }
+        } else {
+            maxLength = 0
+        }
+    }
+    return maxNum
+
+};
+
+console.log(findMaxConsecutiveOnes([1, 1, 1, 1, 1, 0, 1, 1, 1, 1]));//3
