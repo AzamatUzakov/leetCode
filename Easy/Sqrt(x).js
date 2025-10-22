3c985ebfc13e1dd4cefe9e8021b0177715b1e285
AzamatUzakov <azamatakmal053@gmail.com>
2025-10-22 16:42:00 +0500

    Sqrt(x).js

---
+++ Easy/Sqrt(x).js
@@ -0,0 +1,15 @@
+var mySqrt = function (x) {
+
+    let res = 0
+    while (true) {
+        if ((res * res) < x) {
+            res++
+        } else if ((res * res) === x) {
+            return res
+        } else if ((res * res) > x) {
+            return res - 1
+        }
+    }
+};
+
+console.log(mySqrt(4));
