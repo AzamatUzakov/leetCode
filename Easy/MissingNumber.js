956e2858f9d4fd5fe20d6840594d170ed3df41b2
AzamatUzakov <azamatakmal053@gmail.com>
2025-10-25 16:06:48 +0500

    Missing Number

---
+++ Easy/MissingNumber.js
@@ -0,0 +1,14 @@
+var missingNumber = function (nums) {
+
+    let sorted = nums.sort((a, b) => a - b)
+
+    for (let i = 0; i <= sorted.length; i++) {
+        if (i !== sorted[i]) {
+            return i
+        }
+    }
+};
+
+console.log(
+    missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])
+);
